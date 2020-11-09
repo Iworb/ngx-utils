3a73a9a94d1538a56fc47f19162158f6ffee6b9b
Aleksandr Kosheverov <iworb.ne@gmail.com>
2020-11-09 15:35:16 +0300

    [FEAT] Library created

---
+++ projects/iworb/ngx-utils/src/lib/ngx-utils.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NgxUtilsComponent } from './ngx-utils.component';
+
+describe('NgxUtilsComponent', () => {
+  let component: NgxUtilsComponent;
+  let fixture: ComponentFixture<NgxUtilsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NgxUtilsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NgxUtilsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ projects/iworb/ngx-utils/src/lib/ngx-utils.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NgxUtilsComponent } from './ngx-utils.component';
+
+
+
+@NgModule({
+  declarations: [NgxUtilsComponent],
+  imports: [
+  ],
+  exports: [NgxUtilsComponent]
+})
+export class NgxUtilsModule { }
